6de7ccf5fae945ebf28980e602062542932afe9b
Michael Treanor <26148512+skeptycal@users.noreply.github.com>
2021-04-21 04:41:35 -0500

    (GoBot) devtag version 1.3.3.1608849287.391752000-1618996907717168: update examples and deps

---
+++ cmd/examples/readme/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	."github.com/skeptycal/gofile"
+)
+
+const (
+	replRepoName  = []byte("${REPONAME}")
+	replGoVersion = []byte("${GOVERSION}")
+)
+
+func main() {
+
+	f := gofile.
+
+}
