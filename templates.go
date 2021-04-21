fb84f271638b907a182fdfae51b4d38d75ed1799
Michael Treanor <26148512+skeptycal@users.noreply.github.com>
2021-04-21 05:23:43 -0500

    (GoBot) devtag version v0.2.1-1619000588198647: add templates

---
+++ templates.go
@@ -0,0 +1,46 @@
+package gomake
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/skeptycal/gofile"
+)
+
+const templatePath = "template_files"
+
+var (
+	ErrNoTemplatePath       = errors.New("template directory not found")
+	ErrNoTemplate           = errors.New("template file not found")
+	TemplatesAvailable bool = false
+)
+
+type PathError = os.PathError
+
+func ReadTemplate(name string) (string, error) {
+
+	if !TemplatesAvailable {
+		return "", ErrNoTemplatePath
+	}
+
+	templateFileName := filepath.Join(templatePath, name)
+
+	b, err := os.ReadFile(templateFileName)
+	if err != nil {
+		return "", &PathError{"ReadTemplate#os.Readfile", name, err}
+	}
+
+	return "", nil
+}
+
+func init() {
+	if !gofile.IsDir(templatePath) {
+		err := &PathError{"ReadTemplate#gofile.IsDir", templatePath, ErrNoTemplate}
+		log.Info(err)
+		TemplatesAvailable = false
+	} else {
+		TemplatesAvailable = true
+	}
+}
